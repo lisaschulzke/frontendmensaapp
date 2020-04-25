adbcb39b813f0da7fade649c9aa49184b4f8246b
Lisa Schulzke <lisa.schulzke@hfg-gmuend.de>
2020-04-25 14:12:20 +0200

    working with styles as seperate file

--- express-sendfile/webserver.js
+++ express-sendfile/webserver.js
@@ -7,4 +7,8 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/../index.html'));
 });
 
+app.get('/style', function(req, res) {
+    res.sendFile(path.join(__dirname + '/../style.css'));
+});
+
 app.listen(8080);
